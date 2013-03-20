1ba8d1424d8f6ad3a42ba45a959fbb8a47d4e8f9
Gavin Starks <user@localhost.(none)>
2013-03-20 12:23:15 +0000

    integration with leftronic, add gemfile to manage dependencies

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+gem "ruby-trello"
+gem "dotenv"
+gem "leftronicapi"
--- dashboard.rb
+++ dashboard.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby 
 require 'trello'
     require "dotenv"
+    require_relative "leftronic"
+
     Dotenv.load
 
 
@@ -11,17 +13,26 @@ require 'trello'
     end
  
    # Trello todo/doing queues
+    progress=[]
     board = Trello::Board.find(ENV['TRELLO_BOARD'])
-    list = board.lists.each do |list|
+    list = board.lists.first(2).each do |list|
 	puts "#{list.name}: #{list.cards.count}"
-	progress = list.cards.each do |card|
+	list.cards.each do |card|
 		puts card.name
 		checklists = card.checklists.each do |checklist|
 			total = checklist.check_items.count
 		complete = checklist.check_items.select{|item| item["state"]=="complete"}.count
-			puts complete.to_f/total.to_f
+			task_progress =  complete.to_f/total.to_f
+			progress << task_progress
 		end	
 
 	end
 end
+
+    org_progress = progress.inject{ |sum,element| sum += element } / progress.size 
+	puts org_progress
+	
+	id=ENV['LEFTRONIC_DIAL1_ID']
+
     # Publish
+    LeftronicPublisher.perform :number,id,org_progress

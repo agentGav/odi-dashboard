9966e0587bb3c50e81aa941287f8b99283c4313c
Gavin Starks <user@localhost.(none)>
2013-03-20 11:58:19 +0000

    printing total progress per card

--- dashboard.rb
+++ dashboard.rb
@@ -1,16 +1,27 @@
+#!/usr/bin/env ruby 
 require 'trello'
+    require "dotenv"
+    Dotenv.load
+
 
     # Connect
     Trello.configure do |config|
       config.developer_public_key = ENV['TRELLO_DEV_KEY']
       config.member_token = ENV['TRELLO_MEMBER_KEY']
     end
-    # Trello todo/doing queues
-    board = Trello::Board.find(ENV['TRELLO_CLEANUP_BOARD'])
-    todo = board.lists.find{|x| x.name == "To Do"}.cards.count
-    doing = board.lists.find{|x| x.name == "Doing"}.cards.count
+ 
+   # Trello todo/doing queues
+    board = Trello::Board.find(ENV['TRELLO_BOARD'])
+    list = board.lists.each do |list|
+	puts "#{list.name}: #{list.cards.count}"
+	progress = list.cards.each do |card|
+		puts card.name
+		checklists = card.checklists.each do |checklist|
+			total = checklist.check_items.count
+		complete = checklist.check_items.select{|item| item["state"]=="complete"}.count
+			puts complete.to_f/total.to_f
+		end	
+
+	end
+end
     # Publish
-    Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_TRELLO_COUNT'], todo + doing # straight number
-    Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_TRELLO_LINE'], todo + doing # sparkline
-  end
-  

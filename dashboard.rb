06b3ecc32126fa460fd1db0c1e58861ff11e9220
Gavin Starks <user@localhost.(none)>
2013-03-20 17:17:21 +0000

    update to leftronic dashboard

--- dashboard.rb
+++ dashboard.rb
@@ -2,37 +2,36 @@
 require 'trello'
     require "dotenv"
     require_relative "leftronic"
-
     Dotenv.load
-
 
     # Connect
     Trello.configure do |config|
       config.developer_public_key = ENV['TRELLO_DEV_KEY']
-      config.member_token = ENV['TRELLO_MEMBER_KEY']
+#      config.member_token = ENV['TRELLO_MEMBER_KEY']
+	config.member_token = ENV['TRELLO_BOARD_ODI_Dashboard']
+
     end
  
    # Trello todo/doing queues
     progress=[]
     board = Trello::Board.find(ENV['TRELLO_BOARD'])
-    list = board.lists.first(2).each do |list|
-	puts "#{list.name}: #{list.cards.count}"
-	list.cards.each do |card|
-		puts card.name
+    list = board.lists.each do |list|
+    puts "#{list.name}: #{list.cards.count}"
+    list.cards.each do |card|
+    puts card.name
 		checklists = card.checklists.each do |checklist|
 			total = checklist.check_items.count
 		complete = checklist.check_items.select{|item| item["state"]=="complete"}.count
 			task_progress =  complete.to_f/total.to_f
 			progress << task_progress
 		end	
-
 	end
 end
 
-    org_progress = progress.inject{ |sum,element| sum += element } / progress.size 
+    org_progress = 100 * (progress.inject{ |sum,element| sum += element } / progress.size )
 	puts org_progress
 	
-	id=ENV['LEFTRONIC_DIAL1_ID']
+	id=ENV['LEFTRONIC_DIAL2_ID']
 
     # Publish
     LeftronicPublisher.perform :number,id,org_progress
